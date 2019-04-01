48e3ad9043ad729b67f944691a7df17dc3bff570
Károly Taraszovics <tarakarcsi94@gmail.com>
2019-04-01 11:02:25 +0200

    SUBSCRIBE WORKING

--- src/main/java/cmd/Main.java
+++ src/main/java/cmd/Main.java
@@ -15,12 +15,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException, SAXException, ParserConfigurationException {
         XMLParser xmlP = new XMLParser();
-        List<Student> students = xmlP.readStudents("src/main/java/api/Students.xml");
-        List<Subject> subjects = xmlP.getSubjects("src/main/java/api/Subjects.xml");
 
-        Classroom classroom = new Classroom(xmlP.readStudents("src/main/java/api/Students.xml"), xmlP.getSubjects("src/main/java/api/Subjects.xml"));
-
-        System.out.println(xmlP.getSubjects("src/main/java/api/Subjects.xml"));
+        //System.out.println(xmlP.getSubjects("src/main/java/api/Subjects.xml"));
 
         Menu menu = new Menu();
         try {
